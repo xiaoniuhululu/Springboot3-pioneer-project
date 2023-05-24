2d22c2f7b295cdde7a82ecf16dea04b63be6a789
zhangjun <827622690@qq.com>
2023-05-24 14:35:12 +0800

    初始化

---
+++ src/main/java/com/xiaoniuhululu/springboot3pioneerproject/Springboot3PioneerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoniuhululu.springboot3pioneerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3PioneerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3PioneerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoniuhululu/springboot3pioneerproject/Springboot3PioneerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoniuhululu.springboot3pioneerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3PioneerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
